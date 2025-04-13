1d7a27c0080ad5653ca1f592055a405bbf3fa1c5
Airidasurb <airidas.ur1@gmail.com>
2025-04-13 14:05:53 +0300

    additional unit tests

--- app/src/test/java/com/example/myapplication/ExampleUnitTest.java
+++ app/src/test/java/com/example/myapplication/ExampleUnitTest.java
@@ -56,6 +56,54 @@ public class ExampleUnitTest {
     }
 
     @Test
+    public void given_OnlySpaces_When_CountWords_Then_ReturnZero() {
+        String givenString = "     ";
+        int expectedResult = 0;
+        int actualResult = TextAnalyzer.countWords(givenString);
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void given_OnlySpaces_When_CountCharacters_Then_ReturnZero() {
+        String givenString = "     ";
+        int expectedResult = 0;
+        int actualResult = TextAnalyzer.countCharacters(givenString);
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void given_Numbers_When_CountWords_Then_ReturnTwo() {
+        String givenString = "123 456";
+        int expectedResult = 2;
+        int actualResult = TextAnalyzer.countWords(givenString);
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void given_Numbers_When_CountCharacters_Then_ReturnSix() {
+        String givenString = "123 456";
+        int expectedResult = 6;
+        int actualResult = TextAnalyzer.countCharacters(givenString);
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void given_TextWithSymbols_When_CountWords_Then_ReturnTwo() {
+        String givenString = "Labas, pasauli!";
+        int expectedResult = 2;
+        int actualResult = TextAnalyzer.countWords(givenString);
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void given_TextWithSymbols_When_CountCharacters_Then_ExcludeSpacesOnly() {
+        String givenString = "A! b@ C#";
+        int expectedResult = 6; // Pašalinus tarpą, lieka "A!b@C#" (6 simboliai)
+        int actualResult = TextAnalyzer.countCharacters(givenString);
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
     }
